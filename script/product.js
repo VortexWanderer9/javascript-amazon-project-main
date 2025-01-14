28effa5953f30acf27291f3556ddb5c6ec57cab3
Do IT <keshaqvniure99@gmail.com>
2025-01-14 20:55:01 +0545

    Add initial CSS styles and HTML structure for Amazon project pages

---
+++ script/product.js
@@ -0,0 +1,92 @@
+const products = [
+    {
+        image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+        name:'Athletic Cotton Socks 6 Pairs',
+        priceCents: 1090,
+        rating: {
+            stars: 4.5,
+            reviews: 10
+        }, 
+    },
+    {
+        image:'images/products/intermediate-composite-basketball.jpg',
+        name:'Intermediate Size Basketball',
+        priceCents: 2095,
+        rating: {
+            stars: 4,
+            reviews: 127
+        }, 
+    },
+    {
+        image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        name:'Adults Plain Cotton T-Shirt - 2 Pack',
+        priceCents: 795,
+        rating: {
+            stars: 4.5,
+            reviews: 56
+        },
+
+    }
+];
+ let productHtml = '';
+products.forEach(products => {
+    productHtml += `
+     <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${products.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${products.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${products.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+                ${products.rating.reviews}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(products.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>`
+    
+});
+ console.log(productHtml);
+ 
+ const productContainer = document.querySelector('.js-products-grid');
+productContainer.innerHTML = productHtml;
+    
+
+
+
+   
